11d40bb6a9a2d9cb9cc3c6f64780a7c11ba8a6c8
Dovahkiin Huang <qhua948@UoA.auckland.ac.nz>
2018-04-03 16:01:00 +1200

    Work on transform to real world
    - scale bug
    - rotation bug

--- Assets/CalibrationScript.cs
+++ Assets/CalibrationScript.cs
@@ -5,59 +5,70 @@ using UnityEngine;
 using Sanford.Multimedia.Midi;
 using Sanford.Multimedia;
 
-public class CalibrationScript : MonoBehaviour {
+public class CalibrationScript : MonoBehaviour
+{
 
-	public static PianoKey left;
-	public static Vector3 leftThumbPos;
-	public static PianoKey right;
-	public static Vector3 rightThumbPos;
+    public static PianoKey left;
+    public static Vector3 leftThumbPos;
+    public static PianoKey right;
+    public static Vector3 rightThumbPos;
+    public static bool inited = false;
+    public GameObject BlackKey;
+    public GameObject WhiteKey;
 
-	public bool inited = false;
+    void Start()
+    {
+        // Move VR camera
+        var camera = GameObject.FindWithTag("MainCamera");
+        camera.transform.rotation = new Quaternion(0, 0, 0, 1);
+    }
 
-	public GameObject BlackKey;
-	public GameObject WhiteKey;
-
-	// Use this for initialization:w
-
-	void Start () {
-			// Move VR camera
-			var camera = GameObject.FindWithTag("MainCamera");
-		camera.transform.rotation = new Quaternion(0,0,0,1);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-		if(left != null && right != null && !inited) {
+    // Update is called once per frame
+    void Update()
+    {
+        if (left != null && right != null && !inited)
+        {
             var horizontalDist = leftThumbPos.z - rightThumbPos.z;
             var delta = horizontalDist / (right.keyNum - left.keyNum);
-			var camera = GameObject.FindWithTag("MainCamera");
-			var leapSpace = GameObject.FindWithTag("LeapSpace");
-			// Init keyboard
-			Debug.Log("Left = " + left.keyNum + "Right = " + right.keyNum);
-			for (var v = 0; v <= right.keyNum - left.keyNum; v++){
-				var keyType = PianoKeys.GetKeyFor(v+left.keyNum).blackOrWhite;
-				GameObject obj;
-				Vector3 nextPos;
-				if (keyType == BlackOrWhite.White){
-					obj = Instantiate(WhiteKey);
+            var camera = GameObject.FindWithTag("MainCamera");
+            var leapSpace = GameObject.FindWithTag("LeapSpace");
+            // Init keyboard
+            Debug.Log("Left = " + left.keyNum + "Right = " + right.keyNum);
+            Debug.Log("Num keys = " + (right.keyNum - left.keyNum + 1));
+            for (var v = 0; v <= right.keyNum - left.keyNum; v++)
+            {
+                var keyType = PianoKeys.GetKeyFor(v + left.keyNum).blackOrWhite;
+                GameObject obj;
+                Vector3 nextPos;
+                if (keyType == BlackOrWhite.White)
+                {
+                    obj = Instantiate(WhiteKey);
                     nextPos = new Vector3(leftThumbPos.x, leftThumbPos.y, leftThumbPos.z + delta * v);
-				} else {
-					obj = Instantiate(BlackKey);
-                    nextPos = new Vector3(leftThumbPos.x, leftThumbPos.y + 0.1f, leftThumbPos.z + delta * v);
-				}
-				obj.transform.position = nextPos;
-			}
+                }
+                else
+                {
+                    obj = Instantiate(BlackKey);
+                    nextPos = new Vector3(leftThumbPos.x, leftThumbPos.y + 0.01f, leftThumbPos.z + delta * v);
+                }
+                // Assume piano in fixed rotation
+			//	obj.transform.localScale = new Vector3(0.01f,0.01f,delta);
+			//	obj.transform.rotation = new Quaternion(0,0,0,1);
+               // obj.transform.position = nextPos;
+				obj.transform.localPosition = nextPos;
+            }
             // Map game space to real world
-            Debug.Log("Left thumb = " + leftThumbPos + ", right thumb = " + rightThumbPos +
-            	", camera = " + camera.transform.position + ", leap = " + leapSpace.transform.position);
+			var leftPrintZ = string.Format("{0:0.##########}", leftThumbPos.z); 
+			var rightPrintZ = string.Format("{0:0.##########}", rightThumbPos.z); 
+            Debug.Log("Left thumb = " + leftPrintZ + ", right thumb = " + rightPrintZ +
+                ", camera = " + camera.transform.position + ", leap = " + leapSpace.transform.position);
 
-			inited = true;
-		}
+            inited = true;
+        }
 
-		if (inited){
-			// MIDI stuff
-		}
-	}
+        if (inited)
+        {
+            // MIDI stuff
+        }
+    }
 
 }
--- Assets/MidiTest.cs
+++ Assets/MidiTest.cs
@@ -8,61 +8,104 @@ using Leap;
 using Leap.Unity.Attributes;
 
 [RequireComponent(typeof(LeapServiceProvider))]
-public class MidiTest : MonoBehaviour {
+public class MidiTest : MonoBehaviour
+{
 
-	protected InputDevice inputDevice;
-	protected LeapServiceProvider provider;
-	private Frame lastFrame;
+    protected InputDevice inputDevice;
+    protected LeapServiceProvider provider;
+    private Frame lastFrame;
 
-	// Use this for initialization
-	void Start () {
-		if(InputDevice.DeviceCount != 1) {
-			Debug.LogError("Err: No device or too many devices found for MIDI input.");
-			throw new System.Exception();
-		}
-		inputDevice = new InputDevice(0);
-		inputDevice.ChannelMessageReceived += handleChannelMsg;
-		inputDevice.StartRecording();
-		Debug.Log("MIDI device inited");
+    // Use this for initialization
+    void Start()
+    {
+        if (InputDevice.DeviceCount != 1)
+        {
+            Debug.LogError("Err: No device or too many devices found for MIDI input.");
+            throw new System.Exception();
+        }
+        inputDevice = new InputDevice(0);
+        inputDevice.ChannelMessageReceived += handleChannelMsg;
+        inputDevice.StartRecording();
+        Debug.Log("MIDI device inited");
 
         provider = GetComponent<LeapServiceProvider>();
-		provider.OnUpdateFrame += OnUpdateFrame;
-		provider.OnFixedFrame += OnFixedFrame;
-	}
+        provider.OnUpdateFrame += OnUpdateFrame;
+        provider.OnFixedFrame += OnFixedFrame;
+    }
 
-	void OnApplicationQuit(){
-		Debug.Log("MIDI closed");
-		inputDevice.Dispose();
-	}
+    void OnApplicationQuit()
+    {
+        Debug.Log("MIDI closed");
+        inputDevice.Dispose();
+    }
 
-	protected virtual void OnUpdateFrame(Frame frame) {
-		lastFrame = frame;
-	}
-	protected virtual void OnFixedFrame(Frame frame) {
+    protected virtual void OnUpdateFrame(Frame frame)
+    {
+        lastFrame = frame;
+    }
+    protected virtual void OnFixedFrame(Frame frame)
+    {
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
+    }
 
-	void handleChannelMsg(object sender, ChannelMessageEventArgs e) {
-		Debug.Log(e.Message.Command.ToString() + '\t' + '\t' + e.Message.MidiChannel.ToString() + '\t' + e.Message.Data1.ToString() + '\t' + e.Message.Data2.ToString());
-		var obj = GameObject.FindGameObjectWithTag("MainCamera");
-		if(e.Message.Command == ChannelCommand.NoteOn && lastFrame != null) {
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void handleChannelMsg(object sender, ChannelMessageEventArgs e)
+    {
+        Debug.Log(e.Message.Command.ToString() + '\t' + '\t' + e.Message.MidiChannel.ToString() + '\t' + e.Message.Data1.ToString() + '\t' + e.Message.Data2.ToString());
+        var obj = GameObject.FindGameObjectWithTag("MainCamera");
+        if (e.Message.Command == ChannelCommand.NoteOn && lastFrame != null)
+        {
             if (CalibrationScript.left == null)
             {
                 CalibrationScript.left = PianoKeys.GetKeyFor(e.Message.Data1);
-				var fingers = lastFrame.Hands[0].Fingers;
-				CalibrationScript.leftThumbPos= fingers[4].TipPosition.ToVector3();
-				Debug.Log(CalibrationScript.leftThumbPos);
+                CalibrationScript.leftThumbPos = GetThumbPos(lastFrame.Hands[0].Fingers);
+                Debug.Log("Left thumb tip = " + CalibrationScript.leftThumbPos);
             }
             else if (CalibrationScript.right == null)
             {
                 CalibrationScript.right = PianoKeys.GetKeyFor(e.Message.Data1);
-				var fingers = lastFrame.Hands[0].Fingers;
-				CalibrationScript.rightThumbPos = fingers[0].TipPosition.ToVector3();
+                CalibrationScript.rightThumbPos = GetThumbPos(lastFrame.Hands[0].Fingers);
+                Debug.Log("Right thumb tip = " + CalibrationScript.rightThumbPos);
             }
         }
+    }
+
+    private Vector3 GetThumbPos(List<Finger> fingers)
+    {
+        return TransformToCameraVector(ToUnityVector3(GetThumb(fingers).TipPosition));
+    }
+
+
+    private Finger GetThumb(List<Finger> fingers)
+    {
+        foreach (var i in fingers)
+        {
+            if (i.Type == Finger.FingerType.TYPE_THUMB)
+            {
+                return i;
+            }
+        }
+		Debug.LogError("Couldn't find thumb returning null.");
+        return null;
+    }
+
+	private Vector3 ToUnityVector3(Vector vector){
+		return new Vector3(vector.x / 100,
+			vector.y / 100,
+			vector.z / -100);
+	}
+
+	private Vector3 TransformToCameraVector(Vector3 vector){
+        var camera = GameObject.FindWithTag("MainCamera");
+		var cameraVector = camera.transform.position;
+		return new Vector3(vector.x + cameraVector.x,
+			vector.y + cameraVector.y,
+			vector.z + cameraVector.z);
 	}
 }
+
+
