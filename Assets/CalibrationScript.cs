0d8ccc0fda35d7a76fa03fa7377d5e867448c294
Will Molloy <willjoemolloy@gmail.com>
2018-04-06 12:23:22 +1200

    Fix calibration (#1)
    * Work on transform to real world
    
    - scale bug
    - rotation bug
    
    * Fixed calibration(partial)
    
    * Fix calibration may need tweaking e.g. hand is too big.

--- Assets/CalibrationScript.cs
+++ Assets/CalibrationScript.cs
@@ -5,59 +5,64 @@ using UnityEngine;
 using Sanford.Multimedia.Midi;
 using Sanford.Multimedia;
 
-public class CalibrationScript : MonoBehaviour {
+public class CalibrationScript : MonoBehaviour
+{
+    public static PianoKey leftKey;
+    public static Vector3 leftThumbPos;
+    public static PianoKey rightKey;
+    public static bool inited = false;
+    public GameObject BlackKey;
+    public GameObject WhiteKey;
 
-	public static PianoKey left;
-	public static Vector3 leftThumbPos;
-	public static PianoKey right;
-	public static Vector3 rightThumbPos;
+    private float whiteKeyX;
 
-	public bool inited = false;
+    private float blackKeyX;
 
-	public GameObject BlackKey;
-	public GameObject WhiteKey;
+    private const float blackKeyXOffset = 0.005f;
 
-	// Use this for initialization:w
+    private const float blackKeyYOffset = 0.001f;
 
-	void Start () {
-			// Move VR camera
-			var camera = GameObject.FindWithTag("MainCamera");
-		camera.transform.rotation = new Quaternion(0,0,0,1);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-		if(left != null && right != null && !inited) {
-            var horizontalDist = leftThumbPos.z - rightThumbPos.z;
-            var delta = horizontalDist / (right.keyNum - left.keyNum);
-			var camera = GameObject.FindWithTag("MainCamera");
-			var leapSpace = GameObject.FindWithTag("LeapSpace");
-			// Init keyboard
-			Debug.Log("Left = " + left.keyNum + "Right = " + right.keyNum);
-			for (var v = 0; v <= right.keyNum - left.keyNum; v++){
-				var keyType = PianoKeys.GetKeyFor(v+left.keyNum).blackOrWhite;
-				GameObject obj;
-				Vector3 nextPos;
-				if (keyType == BlackOrWhite.White){
-					obj = Instantiate(WhiteKey);
-                    nextPos = new Vector3(leftThumbPos.x, leftThumbPos.y, leftThumbPos.z + delta * v);
-				} else {
-					obj = Instantiate(BlackKey);
-                    nextPos = new Vector3(leftThumbPos.x, leftThumbPos.y + 0.1f, leftThumbPos.z + delta * v);
-				}
-				obj.transform.position = nextPos;
-			}
-            // Map game space to real world
-            Debug.Log("Left thumb = " + leftThumbPos + ", right thumb = " + rightThumbPos +
-            	", camera = " + camera.transform.position + ", leap = " + leapSpace.transform.position);
+    private const float blackKeyZOffset = 0.025f;
 
-			inited = true;
-		}
+    void Start()
+    {
+        whiteKeyX = WhiteKey.transform.localScale.x;
+        blackKeyX = BlackKey.transform.localScale.x;
+    }
 
-		if (inited){
-			// MIDI stuff
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftKey != null && rightKey != null && !inited)
+        {
+            // Init keyboard
+            Debug.Log("Left key = " + leftKey.keyNum + "Right key = " + rightKey.keyNum + "Num keys = " + (rightKey.keyNum - leftKey.keyNum + 1));
+            var currentX = 0f;
+            for (var v = 0; v <= rightKey.keyNum - leftKey.keyNum; v++)
+            {
+                var keyType = PianoKeys.GetKeyFor(v + leftKey.keyNum).blackOrWhite;
+                GameObject obj;
+                Vector3 nextPos;
+                if (keyType == BlackOrWhite.White)
+                {
+                    obj = Instantiate(WhiteKey);
+                    currentX += whiteKeyX;
+                    nextPos = new Vector3(leftThumbPos.x + currentX, leftThumbPos.y, leftThumbPos.z);
+                }
+                else
+                {
+                    obj = Instantiate(BlackKey);
+                    nextPos = new Vector3(leftThumbPos.x + currentX + blackKeyXOffset, leftThumbPos.y + blackKeyYOffset, leftThumbPos.z + blackKeyZOffset);
+                }
+				obj.transform.localPosition = nextPos;
+            }
+            inited = true;
+        }
+
+        if (inited)
+        {
+            // MIDI stuff
+        }
+    }
 
 }
