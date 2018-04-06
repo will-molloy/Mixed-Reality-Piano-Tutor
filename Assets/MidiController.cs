5c3f38e0b4feff4e693d47a7cdc54fa9f92e3993
Will Molloy <willjoemolloy@gmail.com>
2018-04-06 14:50:54 +1200

    Midi colouring (#2)
    * Red colour when key is pressed
    Fixed scene to remove reduenant key
    
    * Refactor colouring code
    
    Also fix x offset of black key now sit between white keys.

---
+++ Assets/MidiController.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sanford.Multimedia;
+using Sanford.Multimedia.Midi;
+using Leap.Unity;
+using Leap;
+using Leap.Unity.Attributes;
+
+[RequireComponent(typeof(LeapServiceProvider))]
+public class MidiController : MonoBehaviour
+{
+    protected InputDevice inputDevice;
+    protected LeapServiceProvider provider;
+    private Frame lastFrame;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (InputDevice.DeviceCount != 1)
+        {
+            Debug.LogError("Err: No device or too many devices found for MIDI input.");
+            throw new System.Exception();
+        }
+        inputDevice = new InputDevice(0);
+        inputDevice.ChannelMessageReceived += handleChannelMsg;
+        inputDevice.StartRecording();
+        Debug.Log("MIDI device inited");
+
+        provider = GetComponent<LeapServiceProvider>();
+        provider.OnUpdateFrame += OnUpdateFrame;
+        provider.OnFixedFrame += OnFixedFrame;
+    }
+
+    void OnApplicationQuit()
+    {
+        Debug.Log("MIDI closed");
+        inputDevice.Dispose();
+    }
+
+    protected virtual void OnUpdateFrame(Frame frame)
+    {
+        lastFrame = frame;
+        foreach (var i in frame.Hands)
+        {
+            foreach (var j in i.Fingers)
+            {
+                Debug.DrawRay(j.TipPosition.ToVector3(), j.Direction.ToVector3(), Color.red);
+            }
+        }
+    }
+    protected virtual void OnFixedFrame(Frame frame)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void handleChannelMsg(object sender, ChannelMessageEventArgs e)
+    {
+        // Handle MIDI event
+        Debug.Log(e.Message.Command.ToString() + '\t' + '\t' + e.Message.MidiChannel.ToString() + '\t' + e.Message.Data1.ToString() + '\t' + e.Message.Data2.ToString());
+        if (e.Message.Command == ChannelCommand.NoteOn && lastFrame != null)
+        {
+            // init piano if needed, left most key first
+            if (Piano.leftMostKey == null)
+            {
+                Piano.leftMostKey = PianoKeys.GetKeyFor(e.Message.Data1);
+                var finger = lastFrame.Hands[0].Fingers[0];
+                Piano.leftThumbPos = GetThumbPos(lastFrame.Hands[0].Fingers);
+                Debug.Log("Left thumb tip = " + Piano.leftThumbPos);
+
+            }
+            else if (Piano.rightMostKey == null)
+            {
+                Piano.rightMostKey = PianoKeys.GetKeyFor(e.Message.Data1);
+            }
+            else
+            {
+                Piano.ActivateKey(e.Message.Data1);
+            }
+        }
+        else if (e.Message.Command == ChannelCommand.NoteOff)
+        {
+            Piano.DeactivateKey(e.Message.Data1);
+        }
+    }
+
+    private Vector3 GetThumbPos(List<Finger> fingers)
+    {
+        return GetThumb(fingers).TipPosition.ToVector3();
+    }
+
+    private Finger GetThumb(List<Finger> fingers)
+    {
+        foreach (var i in fingers)
+        {
+            if (i.Type == Finger.FingerType.TYPE_THUMB)
+            {
+                return i;
+            }
+        }
+        Debug.LogError("Couldn't find thumb returning null.");
+        return null;
+    }
+}
+
+
---
+++ Assets/Piano.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Sanford.Multimedia.Midi;
+using Sanford.Multimedia;
+
+/// <summary>  
+/// Initialises piano and holds state.false
+/// Builds piano along x axis assumes VR is setup perpendicular.
+/// </summary>  
+public class Piano : MonoBehaviour
+{
+    public static PianoKey leftMostKey;
+    public static Vector3 leftThumbPos;
+    public static PianoKey rightMostKey;
+    public static bool isInit;
+    public GameObject BlackKey;
+    public GameObject WhiteKey;
+    private const float blackKeyXOffset = 0.01f;
+    private const float blackKeyYOffset = 0.001f;
+    private const float blackKeyZOffset = 0.025f;
+    private const float whiteKeyWidth = 0.02f;
+    private static readonly Color activationColor = Color.red;
+    private static Dictionary<PianoKey, GameObject> pianoKeys = new Dictionary<PianoKey, GameObject>();
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftMostKey != null && rightMostKey != null && !isInit)
+        {
+            // Init piano
+            Debug.Log("Left key = " + leftMostKey.keyNum + "Right key = " + rightMostKey.keyNum + "Num keys = " + (rightMostKey.keyNum - leftMostKey.keyNum + 1));
+            var currentX = 0f; // Maintain state of current piano width
+            for (var i = leftMostKey.keyNum; i <= rightMostKey.keyNum; i++)
+            {
+                var pianoKey = PianoKeys.GetKeyFor(i);
+                GameObject keyObject;
+                Vector3 nextPos;
+                if (pianoKey.color == KeyColor.White)
+                {
+                    keyObject = Instantiate(WhiteKey);
+                    currentX += whiteKeyWidth;
+                    nextPos = new Vector3(leftThumbPos.x + currentX, leftThumbPos.y, leftThumbPos.z);
+                }
+                else
+                {
+                    keyObject = Instantiate(BlackKey);
+                    nextPos = new Vector3(leftThumbPos.x + currentX + blackKeyXOffset, leftThumbPos.y + blackKeyYOffset, leftThumbPos.z + blackKeyZOffset);
+                }
+                keyObject.transform.localPosition = nextPos;
+                pianoKeys[pianoKey] = keyObject;
+            }
+            isInit = true;
+        }
+    }
+
+    public static void ActivateKey(int keyNum)
+    {
+        if (!isInit)
+        {
+            Debug.Log("Piano not setup.");
+        }
+        else
+        {
+            var pianoKey = PianoKeys.GetKeyFor(keyNum);
+            GameObject gameObject;
+            if (pianoKeys.TryGetValue(pianoKey, out gameObject))
+            {
+                var render = gameObject.GetComponent<MeshRenderer>();
+                render.material.color = activationColor;
+            }
+        }
+    }
+
+
+    public static void DeactivateKey(int keyNum)
+    {
+        if (!isInit)
+        {
+            Debug.Log("Piano not setup.");
+        }
+        else
+        {
+            var pianoKey = PianoKeys.GetKeyFor(keyNum);
+            GameObject gameObject;
+            if (pianoKeys.TryGetValue(pianoKey, out gameObject))
+            {
+                var render = gameObject.GetComponent<MeshRenderer>();
+                render.material.color = pianoKey.color == KeyColor.White ? Color.white : Color.black;
+            }
+        }
+    }
+
+}
