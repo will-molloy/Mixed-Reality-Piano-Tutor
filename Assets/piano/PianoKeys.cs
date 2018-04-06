5b09463a97df607ba26ddf173ec3335f54167ece
Dovahkiin Huang <qhua948@UoA.auckland.ac.nz>
2018-04-06 14:49:27 +1200

    Refactor colouring code
    Also fix x offset of black key now sit between white keys.

--- Assets/piano/PianoKeys.cs
+++ Assets/piano/PianoKeys.cs
@@ -6,68 +6,67 @@ public class PianoKeys {
 
 	static PianoKeys() {
 		keysList = new List<PianoKey>();
-
-		keysList.Add(new PianoKey(36, BlackOrWhite.White));
-		keysList.Add(new PianoKey(37, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(38, BlackOrWhite.White));
-		keysList.Add(new PianoKey(39, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(40, BlackOrWhite.White));
-		keysList.Add(new PianoKey(41, BlackOrWhite.White));
-		keysList.Add(new PianoKey(42, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(43, BlackOrWhite.White));
-		keysList.Add(new PianoKey(44, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(45, BlackOrWhite.White));
-		keysList.Add(new PianoKey(46, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(47, BlackOrWhite.White));
-		keysList.Add(new PianoKey(48, BlackOrWhite.White));
-		keysList.Add(new PianoKey(49, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(50, BlackOrWhite.White));
-		keysList.Add(new PianoKey(51, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(52, BlackOrWhite.White));
-		keysList.Add(new PianoKey(53, BlackOrWhite.White));
-		keysList.Add(new PianoKey(54, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(55, BlackOrWhite.White));
-		keysList.Add(new PianoKey(56, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(57, BlackOrWhite.White));
-		keysList.Add(new PianoKey(58, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(59, BlackOrWhite.White));
-		keysList.Add(new PianoKey(60, BlackOrWhite.White));
-		keysList.Add(new PianoKey(61, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(62, BlackOrWhite.White));
-		keysList.Add(new PianoKey(63, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(64, BlackOrWhite.White));
-		keysList.Add(new PianoKey(65, BlackOrWhite.White));
-		keysList.Add(new PianoKey(66, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(67, BlackOrWhite.White));
-		keysList.Add(new PianoKey(68, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(69, BlackOrWhite.White));
-		keysList.Add(new PianoKey(70, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(71, BlackOrWhite.White));
-		keysList.Add(new PianoKey(72, BlackOrWhite.White));
-		keysList.Add(new PianoKey(73, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(74, BlackOrWhite.White));
-		keysList.Add(new PianoKey(75, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(76, BlackOrWhite.White));
-		keysList.Add(new PianoKey(77, BlackOrWhite.White));
-		keysList.Add(new PianoKey(78, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(79, BlackOrWhite.White));
-		keysList.Add(new PianoKey(80, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(81, BlackOrWhite.White));
-		keysList.Add(new PianoKey(82, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(83, BlackOrWhite.White));
-		keysList.Add(new PianoKey(84, BlackOrWhite.White));
-		keysList.Add(new PianoKey(85, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(86, BlackOrWhite.White));
-		keysList.Add(new PianoKey(87, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(88, BlackOrWhite.White));
-		keysList.Add(new PianoKey(89, BlackOrWhite.White));
-		keysList.Add(new PianoKey(90, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(91, BlackOrWhite.White));
-		keysList.Add(new PianoKey(92, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(93, BlackOrWhite.White));
-		keysList.Add(new PianoKey(94, BlackOrWhite.Black));
-		keysList.Add(new PianoKey(95, BlackOrWhite.White));
-		keysList.Add(new PianoKey(96, BlackOrWhite.White));
+		keysList.Add(new PianoKey(36, KeyColor.White));
+		keysList.Add(new PianoKey(37, KeyColor.Black));
+		keysList.Add(new PianoKey(38, KeyColor.White));
+		keysList.Add(new PianoKey(39, KeyColor.Black));
+		keysList.Add(new PianoKey(40, KeyColor.White));
+		keysList.Add(new PianoKey(41, KeyColor.White));
+		keysList.Add(new PianoKey(42, KeyColor.Black));
+		keysList.Add(new PianoKey(43, KeyColor.White));
+		keysList.Add(new PianoKey(44, KeyColor.Black));
+		keysList.Add(new PianoKey(45, KeyColor.White));
+		keysList.Add(new PianoKey(46, KeyColor.Black));
+		keysList.Add(new PianoKey(47, KeyColor.White));
+		keysList.Add(new PianoKey(48, KeyColor.White));
+		keysList.Add(new PianoKey(49, KeyColor.Black));
+		keysList.Add(new PianoKey(50, KeyColor.White));
+		keysList.Add(new PianoKey(51, KeyColor.Black));
+		keysList.Add(new PianoKey(52, KeyColor.White));
+		keysList.Add(new PianoKey(53, KeyColor.White));
+		keysList.Add(new PianoKey(54, KeyColor.Black));
+		keysList.Add(new PianoKey(55, KeyColor.White));
+		keysList.Add(new PianoKey(56, KeyColor.Black));
+		keysList.Add(new PianoKey(57, KeyColor.White));
+		keysList.Add(new PianoKey(58, KeyColor.Black));
+		keysList.Add(new PianoKey(59, KeyColor.White));
+		keysList.Add(new PianoKey(60, KeyColor.White));
+		keysList.Add(new PianoKey(61, KeyColor.Black));
+		keysList.Add(new PianoKey(62, KeyColor.White));
+		keysList.Add(new PianoKey(63, KeyColor.Black));
+		keysList.Add(new PianoKey(64, KeyColor.White));
+		keysList.Add(new PianoKey(65, KeyColor.White));
+		keysList.Add(new PianoKey(66, KeyColor.Black));
+		keysList.Add(new PianoKey(67, KeyColor.White));
+		keysList.Add(new PianoKey(68, KeyColor.Black));
+		keysList.Add(new PianoKey(69, KeyColor.White));
+		keysList.Add(new PianoKey(70, KeyColor.Black));
+		keysList.Add(new PianoKey(71, KeyColor.White));
+		keysList.Add(new PianoKey(72, KeyColor.White));
+		keysList.Add(new PianoKey(73, KeyColor.Black));
+		keysList.Add(new PianoKey(74, KeyColor.White));
+		keysList.Add(new PianoKey(75, KeyColor.Black));
+		keysList.Add(new PianoKey(76, KeyColor.White));
+		keysList.Add(new PianoKey(77, KeyColor.White));
+		keysList.Add(new PianoKey(78, KeyColor.Black));
+		keysList.Add(new PianoKey(79, KeyColor.White));
+		keysList.Add(new PianoKey(80, KeyColor.Black));
+		keysList.Add(new PianoKey(81, KeyColor.White));
+		keysList.Add(new PianoKey(82, KeyColor.Black));
+		keysList.Add(new PianoKey(83, KeyColor.White));
+		keysList.Add(new PianoKey(84, KeyColor.White));
+		keysList.Add(new PianoKey(85, KeyColor.Black));
+		keysList.Add(new PianoKey(86, KeyColor.White));
+		keysList.Add(new PianoKey(87, KeyColor.Black));
+		keysList.Add(new PianoKey(88, KeyColor.White));
+		keysList.Add(new PianoKey(89, KeyColor.White));
+		keysList.Add(new PianoKey(90, KeyColor.Black));
+		keysList.Add(new PianoKey(91, KeyColor.White));
+		keysList.Add(new PianoKey(92, KeyColor.Black));
+		keysList.Add(new PianoKey(93, KeyColor.White));
+		keysList.Add(new PianoKey(94, KeyColor.Black));
+		keysList.Add(new PianoKey(95, KeyColor.White));
+		keysList.Add(new PianoKey(96, KeyColor.White));
 	}
 
 	public static PianoKey GetKeyFor(int keyNum) {
@@ -75,16 +74,16 @@ public class PianoKeys {
 		return keysList[keyNum-36];
 	}
 }
-public enum BlackOrWhite {
+public enum KeyColor {
 	Black, White
 }
 
 public class PianoKey {
 	public readonly int keyNum;
-	public readonly BlackOrWhite blackOrWhite;
+	public readonly KeyColor color;
 
-	public PianoKey(int keyNum, BlackOrWhite bow) {
+	public PianoKey(int keyNum, KeyColor color) {
 		this.keyNum = keyNum;
-		this.blackOrWhite = bow;
+		this.color = color;
 	}
 }
