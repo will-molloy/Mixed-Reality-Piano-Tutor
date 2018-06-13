202a862f2c4b70041d706b6a189141b721265fb0
Will Molloy <wmol664@UoA.auckland.ac.nz>
2018-06-13 14:13:50 +1200

    Refactor
    - remove leapmotion asset
    - remove unused code (invisible markers etc)
    - change MIDI file to relative path and set in component

---
+++ Assets/Scripts/Sequencer.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Melanchall.DryWetMidi;
+using Melanchall.DryWetMidi.Smf;
+using Melanchall.DryWetMidi.Smf.Interaction;
+using Melanchall.DryWetMidi.Common;
+using System.Linq;
+
+/// <summary>  
+/// - Builds piano roll from MIDI file after virtual piano is built
+/// </summary>  
+[RequireComponent(typeof(Piano))]
+public class Sequencer : MonoBehaviour
+{
+    public string midiFileName;
+
+    public float pianoRollSpeed;
+
+    private MidiFile midiFile;
+
+    private NotesManager noteManager;
+
+    internal Piano piano;
+
+    [SerializeField]
+    public GameObject dropDowns;
+
+    public static Sequencer instance;
+    void Start()
+    {
+        this.piano = GetComponent<Piano>();
+        instance = this;
+        midiFile = MidiFile.Read(midiFileName);
+    }
+
+    public void spawnNotes()
+    {
+        if (noteManager == null)
+        {
+            for (int i = 0; i < midiFile.Chunks.Count; i++)
+            {
+                MidiChunk chunk = midiFile.Chunks[i];
+                if (chunk.GetType().Equals(typeof(TrackChunk)))
+                {
+                    using (var nm = new NotesManager(((TrackChunk)chunk).Events))
+                    {
+                        this.noteManager = nm;
+                    }
+                    break;
+                }
+            }
+        }
+
+        SpawnNotesDropDown(noteManager.Notes.ToList());
+    }
+
+    public void SpawnNotesDropDown(List<Note> notes)
+    {
+        Debug.Log("Spawning piano roll notes");
+        List<GameObject> pianoRollObjects = new List<GameObject>();
+        pianoRollObjects.ForEach(o => GameObject.Destroy(o));
+        pianoRollObjects.Clear();
+        notes.ForEach(e =>
+        {
+            var number = e.NoteNumber;
+            var start = e.Time;
+            var dur = e.Length;
+            float x, y, z;
+            var key = PianoKeys.GetKeyFor(number);
+            if (key == null)
+            {
+                return;
+            }
+            y = start / 1000f;
+            var scale = e.Length / 1000f - 0.01f;
+            piano.GetOffsetForKeyNum(number, out x, out z);
+            var obj = Instantiate(dropDowns);
+            pianoRollObjects.Add(obj);
+            var dropdownScale = obj.transform.localScale;
+            obj.transform.localScale = new Vector3(dropdownScale.x, scale, dropdownScale.z);
+            obj.transform.position = new Vector3(x + 0.0015f, y + 1, z);
+            var renderer = obj.GetComponent<Renderer>();
+            renderer.material.color = key.color == KeyColor.Black ? Color.black : Color.white;
+            var rb = obj.GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(0, -pianoRollSpeed, 0);
+        });
+
+    }
+
+}
