d4b4d8d7e2ada3df231af593604d5f91ed487545
Will Molloy <wmol664@aucklanduni.ac.nz>
2018-06-18 15:01:18 +1200

    Import VRTK library

--- Assets/Scripts/MidiFolderReader.cs
+++ Assets/Scripts/MidiFolderReader.cs
@@ -5,24 +5,28 @@ using System.IO;
 using System;
 using System.Linq;
 
-public class MidiFolderReader : MonoBehaviour {
+public class MidiFolderReader : MonoBehaviour
+{
 
-	public string midiFolderPath;
+    public string midiFolderPath;
 
-	void Start () {
-		Debug.Log("Reading MIDI directory: " + midiFolderPath);
-		processFolder(midiFolderPath);
-	}
-	
-	private static void processFolder(string path){
-		Directory.GetFiles(path).Where(x => x.EndsWith("mid")).ToList()
-		.ForEach(x => processFile(x));		
-	}
+    void Start()
+    {
+        Debug.Log("Reading MIDI directory: " + midiFolderPath);
+        processFolder(midiFolderPath);
+    }
 
-	///<summary>
-	/// Can read database etc. for each tracks scores, difficulty etc.
-	///</summary>
-	private static void processFile(string path){
-		Debug.Log(path);
-	}
+    private static void processFolder(string path)
+    {
+        Directory.GetFiles(path).Where(x => x.EndsWith("mid")).ToList()
+        .ForEach(x => processFile(x));
+    }
+
+    ///<summary>
+    /// Can read database etc. for each tracks scores, difficulty etc.
+    ///</summary>
+    private static void processFile(string path)
+    {
+        Debug.Log(path);
+    }
 }
