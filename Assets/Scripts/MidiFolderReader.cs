240684b1859029c1f63c05560fbec6bf2fab96fe
Will Molloy <wmol664@aucklanduni.ac.nz>
2018-06-20 12:23:00 +1200

    Generating VR scrollpane with midi file names

--- Assets/Scripts/MidiFolderReader.cs
+++ Assets/Scripts/MidiFolderReader.cs
@@ -4,29 +4,77 @@ using UnityEngine;
 using System.IO;
 using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 public class MidiFolderReader : MonoBehaviour
 {
 
-    public string midiFolderPath;
+    [SerializeField]
+    private string midiFolderPath;
+    [SerializeField]
+    private GameObject scrollButtonObj;
+    [SerializeField]
+    private GameObject textObj;
+    private const int buttonSpacing = 10; 
+    private float buttonsHeightSpace = 0;
+    private Rect thisRect;
+
+    private GameObject scrollBarVertical;
 
     void Start()
     {
-        Debug.Log("Reading MIDI directory: " + midiFolderPath);
+        thisRect = this.GetComponent<RectTransform>().rect;
         processFolder(midiFolderPath);
+        this.transform.parent.transform.parent.GetChild(1);
     }
 
-    private static void processFolder(string path)
+    private void processFolder(string midiDir)
     {
-        Directory.GetFiles(path).Where(x => x.EndsWith("mid")).ToList()
+        Debug.Log("Reading MIDI directory: " + midiDir);
+        Enumerable.Range(0, 4).ToList().ForEach(_ => { // TODO remove
+        Directory.GetFiles(midiDir)
+        .Where(x => x.EndsWith(".mid")).ToList()
         .ForEach(x => processFile(x));
+        });
     }
 
     ///<summary>
     /// Can read database etc. for each tracks scores, difficulty etc.
     ///</summary>
-    private static void processFile(string path)
+    private void processFile(string midiPath)
     {
-        Debug.Log(path);
+        var count = this.transform.childCount;
+        Debug.Log("Processing: " + midiPath + ", count: " + count);
+
+        var button = Instantiate(scrollButtonObj);
+        button.transform.SetParent(this.transform);
+        var buttonRect = button.GetComponent<RectTransform>();
+        buttonRect.localPosition = Vector3.zero;
+        buttonRect.localScale = Vector3.one;
+        var buttonOffSetLeft = (thisRect.width - buttonRect.rect.width) / 2;
+        var buttonOffSetTop = count * (buttonSpacing + buttonRect.rect.height);
+        buttonsHeightSpace += buttonOffSetTop;
+        if (buttonsHeightSpace > thisRect.height)
+        {
+            Debug.Log("Increasing canvas height.");
+            // change scrollbar
+        }
+        buttonRect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, buttonOffSetLeft, buttonRect.rect.width);
+        buttonRect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, buttonOffSetTop, buttonRect.rect.height);
+
+
+        var text = Instantiate(textObj);
+        text.transform.SetParent(button.transform);
+        text.GetComponent<UnityEngine.UI.Text>().text = format(midiPath);
+        var textRect = text.GetComponent<RectTransform>();
+        textRect.localPosition = Vector3.zero;
+        textRect.localScale = Vector3.one;
+        textRect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0, buttonRect.rect.width);
+        textRect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 0, buttonRect.rect.height);
+    }
+
+    private string format(string midiPath)
+    {
+        return Regex.Replace(midiPath.Replace(midiFolderPath, "").Replace("mid", "").Replace("_", " "), "[^a-zA-Z ]", "");
     }
 }
